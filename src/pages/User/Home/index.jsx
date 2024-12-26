d30f9d8becdf3a16dd4c7bac6f43ddce85ae3859
Cavid <cavidxh-af106@code.edu.az>
2024-12-26 19:21:42 +0400

    changed

--- src/pages/User/Home/index.jsx
+++ src/pages/User/Home/index.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Home() {
   return (
     <div>
-      Home Page
+      <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Example Image" />
+            <div className="card-body">
+              <h5 className="card-title">Kart 1</h5>
+              <p className="card-text">Bu, kartın qısa təsviridir.</p>
+              <a href="#" className="btn btn-primary">Ətraflı bax</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Example Image" />
+            <div className="card-body">
+              <h5 className="card-title">Kart 2</h5>
+              <p className="card-text">Bu, kartın qısa təsviridir.</p>
+              <a href="#" className="btn btn-primary">Ətraflı bax</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Example Image" />
+            <div className="card-body">
+              <h5 className="card-title">Kart 3</h5>
+              <p className="card-text">Bu, kartın qısa təsviridir.</p>
+              <a href="#" className="btn btn-primary">Ətraflı bax</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
